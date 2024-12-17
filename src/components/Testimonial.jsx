6e060ca5a0fdf4005eff8f00d54d6dbd18a9dc09
Anna Bondarenko <belan_anet@ukr.net>
2024-12-17 13:46:45 +0200

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,31 +1,24 @@
 import React from "react";
 import "../styles/Testimonial.scss";
-import "../images/feedback1.jpg";
-import "../images/feedback2.jpg";
-import "../images/feedback3.jpg";
-import "../images/feedback4.jpg";
-import "../images/feedback5.jpg";
-import "../images/feedback6.jpg";
-import "../images/feedback7.jpg";
 
 const testimonials = [
-  "feedback1.jpg",
-  "feedback2.jpg",
-  "feedback3.jpg",
-  "feedback4.jpg",
-  "feedback5.jpg",
-  "feedback6.jpg",
-  "feedback7.jpg",
+  "../images/feedback1.jpg",
+  "../images/feedback2.jpg",
+  "../images/feedback3.jpg",
+  "../images/feedback4.jpg",
+  "../images/feedback5.jpg",
+  "../images/feedback6.jpg",
+  "../images/feedback7.jpg",
 ];
 
 const Testimonial = ({ currentSlide }) => {
   // Import the image dynamically based on the current slide index
   const imagePath = testimonials[currentSlide];
-  const imageUrl = require(`../images/${imagePath}`);
+  const imageUrl = require(`${imagePath}`);
 
   return (
     <blockquote className="testimonials__item">
-      <img alt={`Скріншот відгука ${currentSlide + 1}`} src={imageUrl} />
+      <img alt="Скріншот відгука" src={imageUrl} />
     </blockquote>
   );
 };
