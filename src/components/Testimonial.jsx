3171a46eb22a1719368bb260fee24fc1b590e1a2
Anna Bondarenko <belan_anet@ukr.net>
2024-12-17 13:53:39 +0200

    v1.0.0

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import "../styles/Testimonial.scss";
 
-const testimonials = [
-  "../images/feedback1.jpg",
-  "../images/feedback2.jpg",
-  "../images/feedback3.jpg",
-  "../images/feedback4.jpg",
-  "../images/feedback5.jpg",
-  "../images/feedback6.jpg",
-  "../images/feedback7.jpg",
-];
+// Контекст для імпорту зображень з папки 'feedbacks'
+const imagesContext = require.context(
+  "../images/feedbacks",
+  false,
+  /\.(jpe?g|png|gif)$/
+);
+const images = imagesContext.keys().map(imagesContext);
 
 const Testimonial = ({ currentSlide }) => {
-  // Import the image dynamically based on the current slide index
-  const imagePath = testimonials[currentSlide];
-  const imageUrl = require(`${imagePath}`);
+  const imageUrl = images[currentSlide];
 
   return (
     <blockquote className="testimonials__item">
-      <img alt="Скріншот відгука" src={imageUrl} />
+      <img alt={`Скріншот відгука ${currentSlide + 1}`} src={imageUrl} />
     </blockquote>
   );
 };
